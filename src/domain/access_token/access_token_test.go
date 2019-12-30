00fae228d3827282b288308da780fc34b54419f2
Brahian <teslasapbe@gmail.com>
2019-12-30 16:59:42 -0300

    Test and access token definition

--- src/domain/access_token/access_token_test.go
+++ src/domain/access_token/access_token_test.go
@@ -1,1 +1,29 @@
 package access_token
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func TestGetNewAccessToken(t *testing.T) {
+	at := GetNewAccessToken()
+	assert.False(t, at.IsExpired(),"brand new access token should not be nil")
+
+	assert.EqualValues(t, "",at.AccessToken, "new access token should not have defined access token id")
+
+	assert.True(t, at.UserId == 0, "new access token should not have an associated user id")
+}
+
+func TestAccessTokenIsExpired(t *testing.T) {
+	at := AccessToken{}
+
+	assert.True(t, at.IsExpired(), "empty access token should be expired by default")
+
+	at.Expires = time.Now().UTC().Add(3 * time.Hour).Unix()
+	assert.False(t, at.IsExpired(), "access token expiring three hours from now should NOT be expired by default")
+}
+
+func TestAccessTokenConstants(t *testing.T) {
+	assert.EqualValues(t, 24, expirationTime)
+}
